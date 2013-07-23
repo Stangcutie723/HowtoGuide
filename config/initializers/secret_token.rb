0e001aab6b6e9f21a08ef2a47ea2108bbafe4dbf
Krystle Frisbie <Stangcutie723@gmail.com>
2013-07-22 20:15:33 -0700

    Adding first files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HowtoGuide::Application.config.secret_key_base = '87627dfbe30448f64a7356f3ef817ad6fa01b1410f3dc3471f0e19272bb356d2e5b81e0d96052302343595e70a84c0054d656df9ee1cd5dca4d96fcd19c5e20f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HowtoGuide::Application.config.session_store :cookie_store, key: '_HowtoGuide_session'
